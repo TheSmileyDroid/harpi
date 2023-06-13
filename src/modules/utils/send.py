3a6bfb5dedd0030b878948993543b7aef5668e94
TheSmileyDroid <ghsilha@gmail.com>
2023-06-13 20:22:43 -0300

    fix embeds again

--- src/modules/utils/send.py
+++ src/modules/utils/send.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord import Embed
 
 
 async def send_message(ctx: commands.Context, message: str | None = None, **kwargs):  # noqa: E501
@@ -7,4 +8,26 @@ async def send_message(ctx: commands.Context, message: str | None = None, **kwar
             while len(message) > 1999:
                 await ctx.send(message[:1999], **kwargs)
                 message = message[1999:]
+            await ctx.send(message, **kwargs)
+            return
+    if 'embed' in kwargs:
+        embed = kwargs['embed']
+        if isinstance(embed, Embed):
+            if len(embed.fields) > 20:
+                while len(embed.fields) > 20:
+                    newembed = Embed(
+                        title=embed.title,
+                        description=embed.description,
+                        color=embed.color)
+                    for field in embed.fields[:20]:
+                        newembed.add_field(
+                            name=field.name,
+                            value=field.value,
+                            inline=field.inline)
+                    newembed.set_footer(text=embed.footer.text)
+                    await ctx.send(embed=newembed)
+                    for _ in range(20):
+                        embed.remove_field(0)
+                await ctx.send(embed=embed)
+                return
     await ctx.send(message, **kwargs)
