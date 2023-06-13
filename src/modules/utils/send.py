93ec4fdf4ca46a1f4dafcc30ea05cc020eb051a2
TheSmileyDroid <ghsilha@gmail.com>
2023-06-13 20:14:01 -0300

    other fix in send message

--- src/modules/utils/send.py
+++ src/modules/utils/send.py
@@ -1,9 +1,10 @@
 from discord.ext import commands
 
 
-async def send_message(ctx: commands.Context, message: str, **kwargs):
-    if len(message) > 1999:
-        while len(message) > 1999:
-            await ctx.send(message[:1999], **kwargs)
-            message = message[1999:]
+async def send_message(ctx: commands.Context, message: str | None = None, **kwargs):  # noqa: E501
+    if message is not None:
+        if len(message) > 1999:
+            while len(message) > 1999:
+                await ctx.send(message[:1999], **kwargs)
+                message = message[1999:]
     await ctx.send(message, **kwargs)
