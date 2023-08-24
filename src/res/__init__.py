282e338c5221383327a2dbe41948dd1d8a295753
TheSmileyDroid <ghsilha@gmail.com>
2023-08-23 22:05:36 -0300

    Colocando de volta do módulo de chat

--- src/res/__init__.py
+++ src/res/__init__.py
@@ -2,6 +2,7 @@ from .music import Music
 from .basic import Basic
 from .dice import Dice
 from .tts import TTS
+from .chat import Chat
 
 async def setup(bot):
     """Make the bot load the cogs.
@@ -13,3 +14,4 @@ async def setup(bot):
     await bot.add_cog(Basic(bot))
     await bot.add_cog(Dice(bot))
     await bot.add_cog(TTS(bot))
+    await bot.add_cog(Chat(bot))
