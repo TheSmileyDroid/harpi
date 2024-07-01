47bb266ba01df99265671da35072fc2a33b2c78c
TheSmileyDroid <ghsilha@gmail.com>
2024-07-01 18:47:44 -0300

    Clear terminal

--- credentials.py
+++ credentials.py
@@ -1,6 +1,7 @@
 from encryption import password_decrypt
 
 password = input("Please write the password: ")
+print("\033[2J")
 
 with open(".credentials", "r") as f:
     DISCORD_TOKEN = password_decrypt(f.readline().encode(), password).decode()
