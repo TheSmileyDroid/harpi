f0349bf254b1ed0f3d4a025e2f5b2b19b9a35634
TheSmileyDroid <ghsilha@gmail.com>
2024-07-18 14:08:30 -0300

    fix

--- src/res/dice.py
+++ src/res/dice.py
@@ -1,7 +1,4 @@
-from dataclasses import dataclass
-from functools import reduce
-import random
-from discord import Embed, Message
+from discord import Embed
 import discord
 from discord.ext import commands
 import discord.ext.commands
@@ -11,34 +8,6 @@ import discord.ext
 
 from .utils.dice_roller import DiceHandler, RandomDiceRoller
 from .utils.send import EmbeddedMessage
-
-
-@dataclass
-class DiceResult:
-    template: str
-    result: int
-
-
-def recursive_roll(text: str, op: int) -> list[DiceResult]:
-    if len(text) == 0:
-        return [DiceResult("", 0)]
-    next_term = min(text.find("+"), text.find("-")) or len(text)
-    if text.find("d") != -1 and text.find("d") > next_term:
-        number_of_dices = int(text[: text.find("d")])
-        number_of_sides = int(text[text.find("d") + 1 : next_term])
-        result = [random.randint(1, number_of_sides) for _ in range(number_of_dices)]
-
-        return [
-            DiceResult(
-                f'{'+' if op else '-'} {number_of_dices}d{number_of_sides} {result}',
-                op * reduce(lambda x, y: x + y, result),
-            )
-        ] + recursive_roll(text[next_term + 1 :], 1 if text[next_term] == "+" else -1)
-    value = int(text[:next_term])
-
-    return [DiceResult(f'{'+' if op else '-'} {value}', op * value)] + recursive_roll(
-        text[next_term + 1 :], 1 if text[next_term] == "+" else -1
-    )
 
 
 class DiceCog(commands.Cog):
