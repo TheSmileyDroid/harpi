000a2a93db0b6d29d4ffe881b3a0e8c93f7bae33
TheSmileyDroid <ghsilha@gmail.com>
2024-11-04 23:59:00 -0300

    more origins

--- app.py
+++ app.py
@@ -98,6 +98,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "http://127.0.0.1:8000",
+        "*.ngrok-free.app",
+        "https://*.ngrok-free.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
