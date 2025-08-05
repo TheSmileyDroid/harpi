df2f744317d852ce3cb55082aaed5ca1489efe5b
TheSmileyDroid <ghsilha@gmail.com>
2025-08-04 23:39:55 -0300

    fix: closes the file after stop playing

--- src/HarpiLib/say.py
+++ src/HarpiLib/say.py
@@ -1,7 +1,9 @@
 import io
+from typing import cast
 
 import discord
 import discord.ext.commands as commands
+from discord.ext.commands.bot import Bot
 from gtts import gTTS
 
 from src.HarpiLib.music_helpers import (
@@ -14,14 +16,17 @@ from src.HarpiLib.musicdata.ytmusicdata import (
 )
 
 
-async def say(ctx: commands.Context, text: str) -> None:
-    voice: discord.VoiceClient = await voice_client(ctx)
+async def say(ctx: commands.Context[Bot], text: str) -> None:
+    voice: discord.VoiceClient = cast(
+        discord.VoiceClient, await voice_client(ctx)
+    )
     if voice.is_playing():
         raise AlreadyPlaying("Já estou reproduzindo algo")
     fp = io.BytesIO()
     tts = gTTS(text=text, lang="pt", tld="com.br")
     tts.write_to_fp(fp)
-    fp.seek(0)
+    _ = fp.seek(0)
     voice.play(
         AudioSourceTracked(FastStartFFmpegPCMAudio(fp, pipe=True)),
+        after=lambda e: fp.close(),
     )
